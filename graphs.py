fc3da565c155b4fceb034f5ff3cdd4d2aaff780e
jeff-dillon <dillonjg@gmail.com>
2022-06-03 15:24:20 -0400

    moved data files, refactored functions

--- graphs.py
+++ graphs.py
@@ -3,27 +3,31 @@ import plotly.express as px
 from pathlib import Path
 
 def load_sales_data() -> pd.DataFrame:
-    """
-    Load the sales data from CSV file.
-    """
-    file_path = Path('sales.csv')
+    file_path = Path('data/sales.csv')
     df = pd.read_csv(file_path)
     return df
 
-def plot_sales(df:pd.DataFrame):
-    """
-    Graph the sales data.
-    :param: df: DataFrame with sales data grouped by Month
-    """
-    fig = px.bar(df, x='Month', y="TotalSales", title='Sales by Month')
+def plot_sales(df:pd.DataFrame) -> None:
+    fig = px.line(df, x='Month', y="TotalSales", title='Sales by Month')
     fig.show()
+    return None
+
+def load_sales_by_artist_data() -> pd.DataFrame:
+    file_path = Path('data/sales_by_artist.csv')
+    df = pd.read_csv(file_path)
+    return df
+
+def plot_sales_by_artist(df:pd.DataFrame):
+    fig = px.bar(df, x='ArtistName', y="TotalSales", title='Sales by Artist')
+    fig.show()
+    return None
 
 def main():
-    """
-    Loads data from CSV files and displays graphs.
-    """
     sales_df = load_sales_data()
     plot_sales(sales_df)
+
+    sales_by_artist_df = load_sales_by_artist_data()
+    plot_sales_by_artist(sales_by_artist_df)
 
 if __name__ == "__main__":
     main()
--- pipeline.py
+++ pipeline.py
@@ -1,11 +1,14 @@
+from multiprocessing.connection import Connection
+import sys
+from pathlib import Path
 import logging
 import sqlite3
 import pandas as pd
-from pathlib import Path
 
-def create_connection(db_file:str):
+
+def create_connection(db_file:str) -> sqlite3.Connection:
     """
-    utility function - creates connection to sqlite database
+    Utility function - creates connection to SQLite database
     :param db_file: database file path
     :return: Connection object or None
     """
@@ -18,17 +21,31 @@ def create_connection(db_file:str):
     return conn
 
 
-def get_sales(conn:sqlite3.Connection) -> pd.DataFrame:
+def configure_logging():
     """
-    Get the list of Track Sales from the database
+    Utility function - configures the data pipeline logging
+    """
+    # configure the logging
+    log_file_path = Path('pipeline_log.txt')
+    logging.basicConfig(filename=log_file_path,
+                        format='%(asctime)s -- [%(levelname)s]: %(message)s', 
+                        datefmt='%m/%d/%Y %I:%M:%S %p',
+                        level=logging.INFO)
+    logging.getLogger().addHandler(logging.StreamHandler(sys.stdout))
+
+
+def get_sales_by_month(conn:sqlite3.Connection) -> pd.DataFrame:
+    """
+    Get total sales by month
     :param: conn: connection to database
-    :return: all genres
+    :return: DataFrame with sales by month (Month, Quantity, TotalSales)
     """
 
     sql_query = """
         SELECT ROUND(SUM(ii.Quantity), 2) as Quantity,
-                ROUND(SUM(ii.UnitPrice), 2) as TotalSales,
-				CAST(strftime('%Y', i.InvoiceDate) as text) || "-" || CAST(strftime('%m', i.InvoiceDate) as text) as Month
+        ROUND(SUM(ii.UnitPrice), 2) as TotalSales,
+        CAST(strftime('%Y', i.InvoiceDate) as text) || "-" || 
+            CAST(strftime('%m', i.InvoiceDate) as text) as Month
         FROM invoice_items ii
         INNER JOIN invoices i ON i.InvoiceId = ii.InvoiceId
 		GROUP BY Month;
@@ -38,25 +55,27 @@ def get_sales(conn:sqlite3.Connection) -> pd.DataFrame:
     return df
     
 
-def get_sales_by_artist(artist_id:int, conn:sqlite3.Connection) -> pd.DataFrame:
+def get_top_artists_by_sales(num_results:int, conn:sqlite3.Connection) -> pd.DataFrame:
     """
-    Get the list of tracks by Artist
-    :param: artist_id: the ID of the artist
+    Get the top N artists by total sales
+    :param: num_results: the number of results to return
     :param: conn: connection to database
-    :return: all tracks in the specified genre
+    :return: DataFrame with sales by Artist (Artist, Quantity, TotalSales)
     """
     sql_query = """
         SELECT ROUND(SUM(ii.Quantity), 2) as Quantity,
         ROUND(SUM(ii.UnitPrice), 2) as TotalSales,
-        CAST(strftime('%Y', i.InvoiceDate) as text) || "-" || CAST(strftime('%m', i.InvoiceDate) as text) as Month
+        ar.Name as ArtistName
         FROM invoice_items ii
         INNER JOIN invoices i ON i.InvoiceId = ii.InvoiceId
         INNER JOIN tracks t ON t.TrackId = ii.TrackId
         INNER JOIN albums al ON al.AlbumId = t.AlbumId
-        GROUP BY Month
-        HAVING al.ArtistId = ?;
+        INNER JOIN artists ar ON ar.ArtistId = al.ArtistId
+        GROUP BY ArtistName
+        ORDER BY TotalSales DESC
+        LIMIT ?
     """
-    df = pd.read_sql_query(sql_query, conn, params=(artist_id, ))
+    df = pd.read_sql_query(sql_query, conn, params=(num_results, ))
     return df
 
 
@@ -65,7 +84,7 @@ def save_sales(sales_df:pd.DataFrame):
     Save the sales data to CSV file
     :param: sales_df: DataFrame with sales data
     """
-    file_path = Path('sales.csv')
+    file_path = Path('data/sales.csv')
     sales_df.to_csv(file_path, index=False)
 
 
@@ -74,38 +93,39 @@ def save_sales_by_artist(sales_by_artist_df:pd.DataFrame):
     Save the sales by artist data to CSV file
     :param: sales_by_artist_df: DataFrame with sales by artist data
     """
-    file_path = Path('sales_by_artist.csv')
+    file_path = Path('data/sales_by_artist.csv')
     sales_by_artist_df.to_csv(file_path, index=False)
 
 
 def main():
-    """
-    Runs the data pipeline.
-    """
-
-    logging.basicConfig(filename='pipeline_log.txt', level=logging.INFO)
-    
-    database = "chinook.db"
+    configure_logging()    
+    logging.info("Starting data pipeline process")
 
     # create a database connection
-    conn = create_connection(database)
+    database_file_path = Path("data/chinook.db")
+    conn = create_connection(database_file_path)
     if not conn:
-        logging.ERROR("error connecting to database.")
+        logging.ERROR("error connecting to database")
         quit()
 
+    # run the data pipeline steps
     with conn:
         
         logging.info("Extracting sales data from database.")
-        sales_df = get_sales(conn)
+        sales_df = get_sales_by_month(conn)
 
         logging.info("Saving the sales data as CSV.")
         save_sales(sales_df)
 
-        logging.info("Extracting Sales by Artist '22'")
-        sales_by_artist_df = get_sales_by_artist(22, conn)
+        logging.info("Extracting top 10 Artists by TotalSales")
+        sales_by_artist_df = get_top_artists_by_sales(10, conn)
 
         logging.info("Saving sales by artist")
         save_sales_by_artist(sales_by_artist_df)
+    
+    conn.close()
+
+    logging.info("Finishing data pipeline process")
 
 
 if __name__ == "__main__":
